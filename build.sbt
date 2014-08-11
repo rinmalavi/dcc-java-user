e434de154a2c8ba772ccc62f03a3f048fc35d4bd
rinmalavi@gmail.com <rinmalavi@gmail.com>
2014-08-11 19:44:59 +0200

    added logback and few irelevanties

--- build.sbt
+++ build.sbt
@@ -1,3 +1,3 @@
-libraryDependencies += "com.dslplatform" % "dsl-client-http" % "0.4.17-URL-SNAPSHOT"
+libraryDependencies += "com.dslplatform" % "dsl-client-http-java" % "0.4.17-URL2-SNAPSHOT"
 
-
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.2"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,33 @@
+import java.util.concurrent.ExecutorService
+
 import com.dslplatform.client.Bootstrap
 import com.dslplatform.test.my.A
 import com.dslplatform.test.my.repositories.ARepository
 
+
 object Main extends App {
-
-  val locator = Bootstrap.init(getClass.getClassLoader.getResourceAsStream("project.props"))
-
-  val arep = locator.resolve(classOf[ARepository])
-
-  arep.insert(new A())
-  arep.insert(new A())
-  arep.insert(new A())
-  arep.insert(new A())
-
-  arep.findAll().get().toArray.foreach({
-    a =>
-      println(a)
-  })
-
-
-
-
+  AA.dosome
 }
 
+object AA {
+  def dosome {
+    val a1: A = new A()
+    val locator = Bootstrap.init(getClass.getClassLoader.getResourceAsStream("project.props"))
+
+    val arep = locator.resolve(classOf[ARepository])
+
+    arep.insert(a1).get()
+    arep.insert(a1).get()
+    arep.insert(a1).get()
+    arep.insert(a1).get()
+
+    arep.findAll().get().toArray.foreach({
+      a =>
+        println(a)
+    })
+
+    locator.resolve(classOf[ExecutorService]).shutdown()
+
+
+  }
+}
