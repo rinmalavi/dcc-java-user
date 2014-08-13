d17e0a5d3ebd565d7ff5cf1e5cf307eae34786b4
rinmalavi@gmail.com <rinmalavi@gmail.com>
2014-08-13 16:44:55 +0200

    Alternative transport examples.

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
-libraryDependencies += "com.dslplatform" % "dsl-client-http-java" % "0.4.17-URL2-SNAPSHOT"
+libraryDependencies += "com.dslplatform" % "dsl-client-http-java" % "0.4.17-1-SNAPSHOT"
 
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.2"
+
+libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.3.5"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,33 +1,46 @@
 import java.util.concurrent.ExecutorService
 
-import com.dslplatform.client.Bootstrap
+import com.dslplatform.client.{AnyTransport, HttpClientTransport, HttpTransport, Bootstrap}
+import com.dslplatform.patterns.ServiceLocator
 import com.dslplatform.test.my.A
 import com.dslplatform.test.my.repositories.ARepository
 
 
 object Main extends App {
-  AA.dosome
+  import Regular._
+  dosome(makeAnyLocator)
+  println("Did some!")
 }
 
-object AA {
-  def dosome {
+object Regular {
+
+  def makeURLocator = {
+    Bootstrap.init(getClass.getClassLoader.getResourceAsStream("project.props"))
+  }
+
+  def makeApacheLocator  = {
+    val initialComponents: java.util.Map[Class[_], Object] = new java.util.HashMap[Class[_], Object]()
+    initialComponents.put(classOf[HttpTransport], classOf[HttpClientTransport])
+    val locator = Bootstrap.init(getClass.getClassLoader.getResourceAsStream("project.props"), initialComponents)
+    locator
+  }
+
+  def makeAnyLocator = {
+    val initialComponents: java.util.Map[Class[_], Object] = new java.util.HashMap[Class[_], Object]()
+    initialComponents.put(classOf[HttpTransport], classOf[AnyTransport])
+    val locator = Bootstrap.init(getClass.getClassLoader.getResourceAsStream("projectAny.props"), initialComponents)
+    locator
+  }
+
+  def dosome(locator: ServiceLocator) {
+
     val a1: A = new A()
-    val locator = Bootstrap.init(getClass.getClassLoader.getResourceAsStream("project.props"))
 
     val arep = locator.resolve(classOf[ARepository])
-
-    arep.insert(a1).get()
-    arep.insert(a1).get()
-    arep.insert(a1).get()
     arep.insert(a1).get()
 
-    arep.findAll().get().toArray.foreach({
-      a =>
-        println(a)
-    })
+    arep.findAll().get().toArray.foreach(println)
 
     locator.resolve(classOf[ExecutorService]).shutdown()
-
-
   }
 }
